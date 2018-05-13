b7f9121f724524553151acf1cad17e1c7768cab4
safitri99 <39227778+safitri99@users.noreply.github.com>
2018-05-13 07:41:56 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,45 @@
+<html>
+<head>
+<title>
+Aplikasi Salesman - Distributor
+</title>
+</head>
+<body>
+<table>
+	<form name="form" method="POST" action="proses_nama.php">
+	<tr>
+		<td> Nama Distributor</td> 
+		<td> : </td>
+		<td><input type="text" name="nd"  /> </td>
+	</tr>
+	<tr>
+		<td> Hutang</td> 
+		<td> : </td>
+		<td><input type="text" name="jh"  /> </td>
+	</tr>
+		<tr>
+		<td> Tanggal Jatuh Tempo</td> 
+		<td> : </td>
+		<td><input type="date" name="tj"  /> </td>
+	</tr>
+		<tr>
+		<td> Nama Sales</td> 
+		<td> : </td>
+		<td><input type="text" name="ns"  /> </td>
+	</tr>
+		<tr>
+		<td> Nomor Chat</td> 
+		<td> : </td>
+		<td><input type="text" name="nc"  /> </td>
+	</tr>
+	<tr>
+		<td></td>
+		<td></td>
+		<td><input type="submit" value="Simpan" name="proses" >
+		<input type="reset" value="Cancel"  name="Cancel"></td>
+	</tr>
+</form>
+
+
+</body>
+</html>
